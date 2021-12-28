08d29d5eef8d51ea5ec597e99298002e90d3ceb4
harlek <zhao_jl@126.com>
2021-12-28 20:23:15 +0800

    24. 两两交换链表中的节点

---
+++ java/src/leetcode/editor/cn/SwapNodesInPairs.java
@@ -0,0 +1,90 @@
+//给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4]
+//输出：[2,1,4,3]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 100] 内 
+// 0 <= Node.val <= 100 
+// 
+// Related Topics 递归 链表 👍 1178 👎 0
+
+
+package leetcode.editor.cn;
+
+public class SwapNodesInPairs {
+    public static void main(String[] args) {
+        Solution solution = new SwapNodesInPairs().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode swapPairs(ListNode head) {
+            //画图即可理解
+            ListNode dummy = new ListNode(-1, head);
+            ListNode p = dummy;
+            while ( p.next != null && p.next.next != null) {
+                ListNode p1 = p.next;
+                ListNode p2 = p.next.next;
+                ListNode p3 = p.next.next.next;
+                p.next = p2;
+                p2.next = p1;
+                p1.next = p3;
+                p = p1;
+            }
+            return dummy.next;
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
